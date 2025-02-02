4755a764da3bb5f3ffb4ac79d1014a3ba30eb261
Clouser <clouser24ryan@gmail.com>
2025-02-02 15:24:45 -0500

    added post request to login route

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -2,7 +2,25 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-  
+  try{
+  const response = await fetch('/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  });
+
+  const data = await response.json();
+
+  if(!response.ok){
+    throw new Error('User info not retreived')
+  }
+  return data;
+} catch (err){
+  console.log('Error from user login', err);
+  return Promise.reject('Could not fetch user info')
+}
 }
 
 
